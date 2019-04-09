20b487fce43eae36391c3266a4658c143bdb4308
thamizhselvan <thamizhtechie@gmail.com>
2019-04-09 12:58:32 +0530

    Updates

---
+++ precache-manifest.42574a2ef83a194e30ad08254cb10a8c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0579845482bbadb6850",
+    "url": "/task-manager-trello/static/js/runtime~main.40ef7a79.js"
+  },
+  {
+    "revision": "e675133f3e4bc9fcbd5e",
+    "url": "/task-manager-trello/static/js/main.367edff3.chunk.js"
+  },
+  {
+    "revision": "458173fc7d2210c40db1",
+    "url": "/task-manager-trello/static/js/2.8b5dfbe2.chunk.js"
+  },
+  {
+    "revision": "e675133f3e4bc9fcbd5e",
+    "url": "/task-manager-trello/static/css/main.1daa3e85.chunk.css"
+  },
+  {
+    "revision": "dc466c85bb45cf764afdfc4295caf631",
+    "url": "/task-manager-trello/index.html"
+  }
+];
